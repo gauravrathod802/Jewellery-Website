3edc428430499b1719120468cd85ff228f024601
Akshay Rathod <8408038901rathod@gmail.com>
2024-02-23 03:25:06 +0530

    footer change

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css'
 import Footer from './Component/Footer/Footer';
 import Header from './Component/Header';
-import CustomerStory from "./Component/CustomerStories/CustomerStory";
 function App() {
 
   return (
     <>
       <Header/>
-      <CustomerStory />
       <Footer/>
     </>
   )
