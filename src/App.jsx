a3c53b4417fc9d79e5817182b4176bdda6e443fe
Akshay Rathod <8408038901rathod@gmail.com>
2024-02-23 23:51:02 +0530

    Review Section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
-import Footer from './Component/Footer/Footer';
 import Header from './Component/Header';
 import Footer from './Component/Footer/Footer';
+import CustomerStory from './Component/CustomerStories/CustomerStory';
 function App() {
 
   return (
     <>
       <Header/>
+      <CustomerStory/>
       <Footer/>
     </>
   )
