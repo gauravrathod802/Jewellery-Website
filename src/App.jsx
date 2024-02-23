0d5591488b171fc0485952b53e0cc72aa45d61a4
gauravrathod802 <98140248+gauravrathod802@users.noreply.github.com>
2024-02-24 01:43:18 +0530

    customer stories

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Header from './Component/Header';
 import Footer from './Component/Footer/Footer';
+import SimpleSlider from './Component/Slider/SimpleSlider';
+import CustomerStory from './Component/CustomerStories/CustomerStory';
 function App() {
 
   return (
     <>
       <Header/>
+      <SimpleSlider/>
+      <CustomerStory/>
       <Footer/>
     </>
   )
