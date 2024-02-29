e69a40cf09667ab5f5806394432d851982246e29
Akshay Rathod <8408038901rathod@gmail.com>
2024-02-29 22:35:25 +0530

    essential and app.jsx file changed to resolve conflict

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,28 @@ import Footer from './Component/Footer/Footer';
 import SimpleSlider from './Component/Slider/SimpleSlider';
 import CustomerStory from './Component/CustomerStories/CustomerStory';
 import RingCategories from './Component/Ring categories/RingCategories';
-import EssentialProduct from './Component/Kalyani Essentials/EssentialProduct';
 function App() {
 
   return (
     <>
       <Header/>
-      <SimpleSlider/>
-      <RingCategories/>
-      <EssentialProduct/>
-      <CustomerStory/>
+      {/* <SimpleSlider/> */}
+      {/* <RingCategories/> */}
+      {/* <CustomerStory/> */}
+      <Routes>
+        <Route path="/" element={
+          <>
+            <SimpleSlider />
+            <RingCategories />
+            <CustomerStory />
+          </>
+          } />
+
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+      </Routes>
       <Footer/>
+
     </>
   )
 }
